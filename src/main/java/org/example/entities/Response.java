80e505f46d197847410679285dfe04bab7fb910f
rema <rima.hleihil@gmail.com>
2022-05-31 19:44:35 +0300

    Response Entity

--- src/main/java/org/example/entities/Response.java
+++ src/main/java/org/example/entities/Response.java
@@ -15,8 +15,41 @@ public class Response  implements Serializable {
     @GeneratedValue
     String respone;
     double financialComp;
-    int compID;
+    int complainID;
 
+    public Response (){
+
+    }
+
+    public Response(String respone, double financialComp, int complainID) {
+        this.respone = respone;
+        this.financialComp = financialComp;
+        this.complainID = complainID;
+    }
+
+    public String getRespone() {
+        return respone;
+    }
+
+    public void setRespone(String respone) {
+        this.respone = respone;
+    }
+
+    public double getFinancialComp() {
+        return financialComp;
+    }
+
+    public void setFinancialComp(double financialComp) {
+        this.financialComp = financialComp;
+    }
+
+    public int getComplainID() {
+        return complainID;
+    }
+
+    public void setComplainID(int complainID) {
+        this.complainID = complainID;
+    }
 }
 
 
