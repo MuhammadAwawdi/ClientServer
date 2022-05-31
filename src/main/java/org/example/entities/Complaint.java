2560ee785abb10f19ba828271cc165e91b46213f
rema <rima.hleihil@gmail.com>
2022-05-31 18:20:53 +0300

    Complaint Entity - update

---
+++ src/main/java/org/example/entities/Complaint.java
@@ -0,0 +1,94 @@
+package org.example.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table
+public class Complaint implements Serializable {
+
+
+    @Id
+    @GeneratedValue
+    private int ComplaintID;
+    private String comp; //complaint's content
+    private String storeName;
+    private Boolean response;
+    private Boolean done;
+    private LocalDate date;
+    private LocalTime sendTime;
+
+    public Complaint() {
+    }
+
+    public Complaint(String comp, String storeName, Boolean response, Boolean done, LocalDate date, LocalTime sendTime) {
+        this.comp = comp;
+        this.storeName = storeName;
+        this.response = response;
+        this.done = done;
+        this.date = date;
+        this.sendTime = sendTime;
+    }
+
+
+    public int getComplaintID() {
+        return ComplaintID;
+    }
+
+    public void setComplaintID(int complaintID) {
+        ComplaintID = complaintID;
+    }
+
+    public String getComp() {
+        return comp;
+    }
+
+    public void setComp(String comp) {
+        this.comp = comp;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public Boolean getResponse() {
+        return response;
+    }
+
+    public void setResponse(Boolean response) {
+        this.response = response;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getSendTime() {
+        return sendTime;
+    }
+
+    public void setSendTime(LocalTime sendTime) {
+        this.sendTime = sendTime;
+    }
+}
