297c7d494c61ec059490fcc025d27172d33b6223
rema <rima.hleihil@gmail.com>
2022-06-01 17:14:47 +0300

    AdvancedMsg Entity

---
+++ src/main/java/org/example/entities/AdvancedMsg.java
@@ -0,0 +1,38 @@
+package org.example.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdvancedMsg implements Serializable {
+    private static final long serialVersionUID = -8224097662914849956L;
+    String msg;
+    List<Object> objectList;
+
+    public AdvancedMsg() {
+        objectList = new ArrayList<>();
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public AdvancedMsg(String msg) {
+        this.msg = msg;
+        this.objectList = new ArrayList<>();
+    }
+
+    public List<Object> getObjectList() {
+        return objectList;
+    }
+
+    public void addobject(Object obj) {
+        this.objectList.add(obj);
+    }
+}
+
+
